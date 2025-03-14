ad6bab978759ff3a022fd2ea6f98cf8e815ec4c6
Rimaki Taema <txwfyxa@gmail.com>
2025-03-14 14:05:49 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-python -m nuitka --standalone --onefile --disable-ccache --output-dir=build convert.py
-mv build/convert.bin build/convmusic
-chmod +x build/convmusic
+python setup.py build
+# mv build/convert.bin build/convmusic
+# chmod +x build/convmusic
